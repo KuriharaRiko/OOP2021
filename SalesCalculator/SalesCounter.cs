ad2117bdba13a8f3d605db0499a2692900871f83
KuriharaRiko <32235@ojs.ac.jp>
2021-05-27 15:21:02 +0900

    p68  初版の完成

---
+++ SalesCalculator/SalesCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    // 売上集計クラス
+    class SalesCounter {
+        
+        private List<Sale> _sales;
+
+        // コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+        // 店舗別売上を求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName)) 
+                    // 既にコレクションに店舗が設定されている
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    // コレクションへ店舗を登録
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
+

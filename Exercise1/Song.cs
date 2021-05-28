221a0322a881b8fa8fd7bf5cc849d08f1c550e04
KuriharaRiko <32235@ojs.ac.jp>
2021-05-28 15:20:14 +0900

    p79 問題2.1.2

--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     public class Song {
-        public string Title { get; private set; }
-        public string ArtistName { get; private set; }
-        public int Length { get; private set; }
-
         
+        public string Title { get; private set; }   // 歌のタイトル
+        public string ArtistName { get; private set; }  // アーティスト名
+        public int Length { get; private set; } // 演奏時間（秒）
 
+
+        // コンストラクタ
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
         
-
     }
 }

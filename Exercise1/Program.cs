b7b431b52461384c5e9980e5163f7e2c3791dd2e
KuriharaRiko <32235@ojs.ac.jp>
2021-05-28 15:13:08 +0900

    p79 問題2.1.1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            // 2.1.3
+        }
+
+        // 2.1.4
+        static void PrintSongs(string[] args) {
         }
     }
 }
---
+++ Exercise1/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    public class Song {
+        public string Title { get; private set; }
+        public string ArtistName { get; private set; }
+        public int Length { get; private set; }
+
+        
+
+        
+
+    }
+}

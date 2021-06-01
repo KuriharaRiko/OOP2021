b42c9a69cfc7a76e223baec2a47cb570e4293c01
KuriharaRiko <32235@ojs.ac.jp>
2021-06-01 10:29:19 +0900

    p79 問題2.2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -13,13 +13,19 @@ namespace Exercise2 {
                 PrintInchToMeterList(1, 10);
             }
         }
-
-        private static void PrintInchToMeterList(int start, int stop) {
-            
-        }
-
+        // インチからメートルへの対応表を出力
         private static void PrintMeterToInchList(int start, int stop) {
-            
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+        // メートルからインチへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+            }
         }
     }
 }

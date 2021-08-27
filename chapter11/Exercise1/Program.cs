ded4dfcdd39faf0234716a179f22ccc016a2cf71
璃子 栗原 <32235@ojs.ac.jp>
2021-08-27 15:59:44 +0900

    問題11.1.2 完成

---
+++ chapter11/Exercise1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args){
+            var file = "Sample.xml";
+           
+            Exercise1_1(file);
+            Console.WriteLine("------");
+
+            Exercise1_2(file);
+            Console.WriteLine("------");
+
+            Exercise1_3(file);
+            Console.WriteLine("------");
+        }
+
+        private static void Exercise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                             .Select(x => new 
+                             {
+                              Name = x.Element("name").Value,
+                              Teammembers = x.Element("teammenbers").Value
+                             });
+            foreach (var sport in sports){
+                Console.WriteLine("{0} {1}", sport.Name, sport.Teammembers);
+            }
+        }
+
+        private static void Exercise1_2(string file)
+        {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                            .Select(x => new
+                            {
+                                Firstplayed = x.Element("firstplayed").Value,
+                                Name = x.Element("name").Attribute("kanji").Value
+                            }).OrderBy(x => int.Parse(x.Firstplayed)); ;
+            foreach (var sport in sports){
+                Console.WriteLine("{0} {1}", sport.Name, sport.Firstplayed);
+            }
+
+        }
+
+        private static void Exercise1_3(string file)
+        {
+            var xdoc = XDocument.Load(file);
+        }
+    }
+}

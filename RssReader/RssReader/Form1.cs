f2f06ba63d5d73adbba9e61b6ab7f557789d2efa
璃子 栗原 <32235@ojs.ac.jp>
2021-09-06 16:00:57 +0900

    タイトル表示未完成

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            setRasTitle(tbUrl.Text);
+        }
+
+        // 指定したURL先からXMLデータを取得しtitle要素を取得し、リストボックスへセットする
+        private void setRasTitle(string url)
+        {
+            
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                
+            }
+        }   
+    }
+}

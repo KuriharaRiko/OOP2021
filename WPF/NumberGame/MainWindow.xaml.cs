75dd5804bcbd61c3e716e4e6139bc1e40d49de2e
璃子 栗原 <32235@ojs.ac.jp>
2021-11-12 15:57:05 +0900

    数あてゲーム　わからない

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt1 = e.Source as Button;
+            bt1.Background = Brushes.Aqua;
+
+            if (bt1 == seikai)
+            {
+                TextBlock tb = ;
+                break;
+            }
+        }
+    }
+}
---
+++ WPF/NumberGame/NumberGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGame
+{
+    public class NumberGame: MainWindow
+    {
+        int seikai = new Random().Next(1, 26);
+        int count = 1;
+    }
+}
